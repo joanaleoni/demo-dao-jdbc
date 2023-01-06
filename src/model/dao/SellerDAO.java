9b1ee4458556d7e25cdb5f0911cea49bcfbcb109
Joana Leoni Pereira <joanaleonipereira@gmail.com>
2023-01-06 03:02:46 -0300

    DepartmentDAO and SellerDAO interfaces

---
+++ src/model/dao/SellerDAO.java
@@ -0,0 +1,16 @@
+package model.dao;
+
+import java.util.List;
+import model.domain.Seller;
+
+/**
+ *
+ * @author joana
+ */
+public interface SellerDAO {
+    void insert(Seller seller);
+    void update(Seller seller);
+    void deleteById(Integer id);
+    Seller findById(Integer id);
+    List<Seller> findAll();
+}
